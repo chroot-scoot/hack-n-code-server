55e32be3cefb188c0c0ab558ca31e718ab83f3f7
Abhinav Pandey <abhinav.pandey.1512@gmail.com>
2024-04-06 17:09:05 +0530

    fix: gitignore .env

--- src/common/config.ts
+++ src/common/config.ts
@@ -4,13 +4,10 @@ config();
 // Server Operations
 export const environment = process.env.NODE_ENV ?? 'development';
 export const port = process.env.PORT ?? 8080;
-export const cacheTime = process.env.CACHE_TIME ?? 0;
-export const databaseURI =
-  (environment == 'development'
-    ? process.env.DATABASE_URI_DEV
-    : process.env.DATABASE_URI_PROD) ?? '';
+export const cacheTime = process.env.CACHE_TTL ?? 0;
+export const databaseURI = process.env.DATABASE_URI ?? '';
 
 // Security
 export const allowedOrigin = process.env.ALLOWED_ORIGIN ?? '*';
-export const jwtTokenSecret = process.env.AUTH_TOKEN_SECRET ?? '';
-export const jwtTokenLifetime = process.env.AUTH_TOKEN_LIFETIME ?? '1h';
+export const jwtTokenSecret = process.env.JWT_SECRET ?? '';
+export const jwtTokenLifetime = process.env.JWT_TTL ?? '1h';
